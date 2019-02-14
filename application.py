29ae92a14213bdb0bc122a75d1292cb8305037fb
Aakash Wabale <keshav22495@gmail.com>
2019-02-14 17:45:20 +0530

    index fun added

--- application.py
+++ application.py
@@ -1,14 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    headline="Goodbye"
-    return render_template("index.html",headline=headline)
+    
+    return "Hello Aakash"
 
-@app.route("/<string:name>")
-def aki(name):
-    name=name.capitalize()
-    return f"<h1>Hello, {name}!!!!</h1>"
+
 
 
